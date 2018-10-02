d8eb7ef2791e346009cebf07c27127b314ed1572
DESKTOP-R5IU7NV\yosoy <daniel.rabasco.garcia@alumnos.upm.es>
2018-10-02 12:33:21 +0200

    Entrega_Daniel_Rabasco

--- src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
+++ src/main/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifier.java
@@ -1,9 +1,105 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
+import java.util.ArrayList;
+
 public class SudokuVerifier 
 {
-	public int verify (String candidateSolution)
-    {
-        return 0;
-    }
+public int verify(String candidateSolution){
+		
+		Group[] columnas= new Group[9];
+		Group[] filas = new Group[9];
+		Group[] subgrid = new Group[9];
+		
+		int cont = 0;
+		
+		for (int i = 0; i < 9; i++){
+			filas[i] = new Group();
+			columnas[i] = new Group();
+			subgrid[i] = new Group();
+		}
+		
+		for (int i = 0; i < 9; i++){
+			for (int j = 0; j < 9; j++){
+				if (!Character.isDigit(candidateSolution.charAt(cont)) || candidateSolution.charAt(cont) == '0'){
+					return -1;
+				}
+				else{
+					if (!filas[i].AddNumber(candidateSolution.charAt(cont))){
+						return -3;
+					}					
+					else if (!columnas[j].AddNumber(candidateSolution.charAt(cont))){
+						return -4;
+					}
+					
+					if (i < 3 && j < 3){
+						if (!subgrid[0].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (i < 3 && 2 < j && j < 6){
+						if (!subgrid[1].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (i < 3 && 5 < j){
+						if (!subgrid[2].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (2 < i && i < 6 && j < 3){
+						if (!subgrid[3].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (2 < i && i < 6 && 2 < j && j < 6){
+						if (!subgrid[4].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (2 < i && i < 6 && j > 5){
+						if (!subgrid[5].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (i > 5 && j < 3){
+						if (!subgrid[6].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (i > 5 && 2 < j && j < 6){
+						if (!subgrid[7].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					else if (i > 5 && j > 5){
+						if (!subgrid[8].AddNumber(candidateSolution.charAt(cont))){
+							return -2;
+						}
+					}
+					
+					cont++;
+				}
+			}
+		}
+		return 0;
+	}
+	
+	public class Group {
+		
+		ArrayList<Character> group;
+		
+		public Group(){
+			group = new ArrayList<Character>();
+		}
+		
+		public boolean AddNumber(char number){
+			if (group.contains(number)){
+				return false;				
+			}
+			else{
+				group.add(number);
+				return true;
+			}
+		}
+	}
 }
--- src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
+++ src/test/java/es/upm/grise/profundizacion2018/sudokuverifier/SudokuVerifierTest.java
@@ -1,13 +1,56 @@
 package es.upm.grise.profundizacion2018.sudokuverifier;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class SudokuVerifierTest {
     
+	SudokuVerifier sd = new SudokuVerifier();
+	
 	@Test
-	public void test() {
-    		fail("Not yet implemented");
-    }
+	public void sudoku_bien_devuelve_0() {
+		assertEquals(0, 
+				sd.verify("915384276" +
+						"843726915" +
+						"762591483" +
+						"698257341" +
+						"327148569" +
+						"154639827" +
+						"436972158" +
+						"279815634" +
+						"581463792"));
+	}
+	
+	@Test
+	public void sudoku_fila_mal_devuelve_menos3() {
+		assertEquals(-3, 
+				sd.verify("915394276" +
+						"843726915" + 
+						"762591483" + 
+						"698257341" +
+						"327148569" +
+						"154639827" +
+						"436972158" +
+						"279815634" +
+						"581463792"));
+	}
+
+	@Test
+	public void sudoku_columna_mal_devuelve_menos4() {
+		assertEquals(-4, 
+				sd.verify("915384276943726915762591483698257341327148569154639827436972158279815634581463792"));
+	}
+	
+	@Test
+	public void sudoku_grid_mal_devuelve_menos2() {
+		assertEquals(-2, 
+				sd.verify("915384276893726415762591483698257341327148569154639827436972158279815634581463792"));
+	}
+	
+	@Test
+	public void sudoku_con_cero_devuelve_menos1() {
+		assertEquals(-1, 
+				sd.verify("915384276843726915762591083698257341327148569154639827436972158279815634581463792"));
+	}
 }
